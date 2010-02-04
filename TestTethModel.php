832de576eb74e3c25dcb9b6c705c05d04192a582
charlesmarshall <cm56marshall@googlemail.com>
2010-02-04 23:34:12 +0000

    change that a bit so can handle more later

--- TestTethModel.php
+++ TestTethModel.php
@@ -3,19 +3,21 @@ class TestTethModel extends BaseTest{
   public $class = "TethModel";
   
   public function setter(){
+    $ret = false;
     $test_model = new TethModel();
     $test_model->test_col = "test_value";
-    if($test_model->data["test_col"] == "test_value"){
-      $this->results['setter']['basic_string_value'] = true;
-      return true;
-    }else return false;
+    if($test_model->data["test_col"] != "test_value") $this->results['setter']['basic_string_value'] = $ret = false;
+    else $this->results['setter']['basic_string_value'] = true;
+
+    return $ret;
   }
   public function getter(){
+    $ret = true;
     $test_model = new TethModel();
     $test_model->test_col = "test_value";
-    if($test_model->test_col == "test_value"){
-      $this->results['getter']['basic_string_value'] = true;
-      return true;
-    }else return false;
+    if($test_model->test_col != "test_value") $this->results['getter']['basic_string_value'] = $ret = false;
+    else $this->results['getter']['basic_string_value'] = true;
+
+    return $ret;
   }
 }?>
