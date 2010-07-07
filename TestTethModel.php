e0fa071803444c87904d9a201a44e324f2e04487
charlesmarshall <cm56marshall@googlemail.com>
2010-07-07 21:14:13 +0100

    swap to use class name var

--- TestTethModel.php
+++ TestTethModel.php
@@ -3,7 +3,7 @@ class TestTethModel extends BaseTest{
   public $class = "TethModel";
   
   public function construct(){
-    $test_model = new TethModel();
+    $test_model = new $this->class();
     $this->results['construct']['empty'] = ($test_model->data == array());
     $test_model = new TethModel(array("testcol1"=>"test1","testcol2"=>"test2"));
     $this->results['construct']['with_data'] = ($test_model->data == array("testcol1"=>"test1","testcol2"=>"test2"));
@@ -11,13 +11,13 @@ class TestTethModel extends BaseTest{
   }
   
   public function setter(){
-    $test_model = new TethModel();
+    $test_model = new $this->class();
     $test_model->test_col = "test_value";
     return $this->results['setter']['basic_string_value'] = ($test_model->data["test_col"] == "test_value");
   }
   
   public function getter(){
-    $test_model = new TethModel();
+    $test_model = new $this->class();
     $test_model->test_col = "test_value";
     return $this->results['getter']['basic_string_value'] = ($test_model->test_col == "test_value");
   }
